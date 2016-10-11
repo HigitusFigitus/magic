fbeff67a802c7a0a59551cf33093e2f2eeb990cf
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-11 12:25:05 -0700

    Modifies xhr request logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,7 @@
 get '/' do
   @card = Card.find(random_id)
   if request.xhr?
-    ### not sure about this
-    return card.image_url, layout: false
+    erb :"_image_partial", layout: false
   else
     erb :'index'
   end
