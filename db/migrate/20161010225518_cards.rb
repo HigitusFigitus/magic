33cf457380d37b0d09f5e0deec8a9d74bf52ef27
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-12 22:23:47 -0700

    Adds migrations with users and favorites table

--- db/migrate/20161010225518_cards.rb
+++ db/migrate/20161010225518_cards.rb
@@ -1,8 +1,8 @@
 class Cards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.integer :multiverse_id, null: false
-      t.string  :image_url, null: false
+      t.integer  :multiverse_id, null: false
+      t.string   :image_url, null: false
     end
   end
 end
