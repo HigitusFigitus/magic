9e1b9dac6e329f87290e04c47f14b11bc3562b00
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-12 22:25:51 -0700

    Adds user and favorite models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ActiveRecord::Base
+  has_many :favorites
+  has_many :users, through: :favorites
+
   validates :image_url, presence: true
   validates :multiverse_id, presence: true
 end
