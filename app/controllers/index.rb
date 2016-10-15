d3b932279cd9ccf2dfe91f1f22929047f6f02b47
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-15 15:13:10 -0700

    Adds redirects from root

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,6 @@
 get '/' do
-  @card = Card.find(random_id)
-  if request.xhr?
-    erb :'_image_partial', layout: false
-  else
-    erb :'index'
-  end
+  redirect '/cards'
 end
 
-get '/users/new' do
-  erb :'users/new'
-end
 
-get '/sessions/new' do
-  erb :'sessions/new'
-end
+
