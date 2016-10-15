868a8c2c64ca70ba6ad1b2296363bdcb6b70d9a8
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-15 15:14:59 -0700

    Adds user registration and users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  if params[:password_confirmation] == params[:user][:password]
+
+    @user = User.new(params[:user])
+
+    if @user.save!
+      session[:id] = user.id
+      redirect '/users/#{user.id}'
+    else
+      @errors = @user.errors.full_messages
+      erb :'users/new'
+    end
+
+  else
+    @errors = ["Email or password don't match."]
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
