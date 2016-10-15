82e8c40bc9abeb15287d7eb0642ccd0d8b8447d4
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-15 15:14:02 -0700

    Fixes spacing in models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
-  has_many :favorites
-  has_many :users, through: :favorites
+  has_many  :favorites
+  has_many  :users, through: :favorites
 
   validates :image_url, presence: true
   validates :multiverse_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :favorites
   has_many :cards, through: :favorites
+
+   def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
