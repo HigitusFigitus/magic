57c474aaec1d52f10586f6980620355b14628d34
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-21 02:03:13 -0700

    Adds todo list

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,5 +2,9 @@ get '/' do
   redirect '/cards'
 end
 
-
-
+# To do:
+# - Make the background image always display
+#   - Bonus: use Twitter bootstrap to automagically do this for desktop and mobile
+# - Fix Home button on Leaderboard page
+# - Make Logout button work
+# - Fix Scry AJAX
