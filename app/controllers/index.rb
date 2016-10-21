ac4b330c959e0297359285c8edded36ff0f41d99
Ilaria Varriale <ilariasvarriale@gmail.com>
2016-10-21 10:31:54 -0700

    Updates todo list

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,8 +3,9 @@ get '/' do
 end
 
 # To do:
-# - Make the background image always display
-#   - Bonus: use Twitter bootstrap to automagically do this for desktop and mobile
 # - Fix Home button on Leaderboard page
 # - Make Logout button work
 # - Fix Scry AJAX
+
+# - Make the background image always display
+#   - Bonus: use Twitter bootstrap to automagically do this for desktop and mobile
